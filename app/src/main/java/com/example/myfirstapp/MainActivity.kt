3aee23f71ad0e528b2b548b017f2f7249b63e8aa
mrGoonies <mrgoonies@mrGooniess-MacBook-Pro.local>
2022-06-23 23:18:34 -0400

    finished application

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+        rollButton.setOnClickListener {
+            val rand = java.util.Random().nextInt(seekBar.progress) + 1
+            resultTextView.text = rand.toString()
+        }
+    }
+}
